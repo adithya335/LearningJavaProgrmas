ebc420482b4faf19fa72765741188692b6fcb395
ADITHYA <sampathadithya11@gmail.com>
2025-06-10 17:00:17 +0530

    june 10th learning

---
+++ src/tasks/June10thTask.java
@@ -0,0 +1,18 @@
+package tasks;
+
+public class June10thTask {
+
+    public static void main(String[] args) {
+        System.out.printf("%d*%d=%d%n",9,1,9);
+        System.out.printf("%d*%d=%d%n",9,2,18);
+        System.out.printf("%d*%d=%d%n",9,3,27);
+        System.out.printf("%d*%d=%d%n",9,4,36);
+        System.out.printf("%d*%d=%d%n",9,5,45);
+        System.out.printf("%d*%d=%d%n",9,6,54);
+        System.out.printf("%d*%d=%d%n",9,7,63);
+        System.out.printf("%d*%d=%d%n",9,8,72);
+        System.out.printf("%d*%d=%d%n",9,9,81);
+        System.out.printf("%d*%d=%d%n",9,10,90);
+
+    }
+}
