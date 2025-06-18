0cb87ba1aa9c966576034a982bbadf7dc022713c
ADITHYA <sampathadithya11@gmail.com>
2025-06-18 10:13:13 +0530

    Added alternate way in comments for leap year checker

--- src/june18thTask/LeapYearChecker.java
+++ src/june18thTask/LeapYearChecker.java
@@ -24,5 +24,17 @@ public class LeapYearChecker {
         else{
             System.out.println("Year "+year+" is not a leap year");
         }
+
+        //public static boolean isLeapYear(int year) {
+        //        if (year % 4 != 0) {
+        //            return false;
+        //        } else if (year % 100 != 0) {
+        //            return true;
+        //        } else if (year % 400 != 0) {
+        //            return false;
+        //        } else {
+        //            return true;
+        //        }
+        //    }
     }
 }
