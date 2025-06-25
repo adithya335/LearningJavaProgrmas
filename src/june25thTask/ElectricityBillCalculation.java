f714d22792b77dbb10cbb1991d95064afe483634
ADITHYA <sampathadithya11@gmail.com>
2025-06-25 19:53:07 +0530

    25th electricity bill updates

--- src/june25thTask/ElectricityBillCalculation.java
+++ src/june25thTask/ElectricityBillCalculation.java
@@ -8,31 +8,46 @@ public class ElectricityBillCalculation {
         System.out.println("Enter the number of units");
         int units = new Scanner(System.in).nextInt();
         double cost = 0;
-        int loop = 0;
-        while (units > 0) {
-            if (loop == 0) {
-                cost += (100 * 0.5);
-                units -= 100;
-                loop++;
-                continue;
-            }
-            if (loop == 1) {
-                cost += (100 * 0.75);
-                units -= 100;
-                loop++;
-                continue;
-            }
-            if (loop == 2) {
-                cost += (100 * 1.2);
-                units -= 100;
-                loop++;
-                continue;
-            }
-            if (loop == 3) {
-                cost += (units * 1.5);
-                break;
-            }
+//        int loop = 0;
+//        while (units > 0) {
+//            if (loop == 0) {
+//                cost += (100 * 0.5);
+//                units -= 100;
+//                loop++;
+//                continue;
+//            }
+//            if (loop == 1) {
+//                cost += (100 * 0.75);
+//                units -= 100;
+//                loop++;
+//                continue;
+//            }
+//            if (loop == 2) {
+//                cost += (100 * 1.2);
+//                units -= 100;
+//                loop++;
+//                continue;
+//            }
+//            if (loop == 3) {
+//                cost += (units * 1.5);
+//                break;
+//            }
+//        }
+
+        if (units > 300) {
+            units -= 300;
+            cost = (100 * 0.5) + (100 * 0.75) + (100 * 1.2) + (units * 1.5);
+        } else if (units > 200) {
+            units -= 200;
+            cost = (100 * 0.5) + (100 * 0.75) + (units * 1.2);
+        } else if (units > 100) {
+            units -= 100;
+            cost = (100 * 0.5) + (units * 0.75);
+        } else {
+            cost = units * 0.5;
         }
+
+
         System.out.println(cost);
     }
 }
