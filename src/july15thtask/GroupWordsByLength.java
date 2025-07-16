7542f30f7ae4184f4511e99b02307e1582dbd8a6
ADITHYA <sampathadithya11@gmail.com>
2025-07-16 06:49:40 +0530

    enhanced group words by length

--- src/july15thtask/GroupWordsByLength.java
+++ src/july15thtask/GroupWordsByLength.java
@@ -8,23 +8,11 @@ public class GroupWordsByLength {
 
     public static void main(String[] args) {
         String[] input = {"Java", "is", "fun", "cool", "Hi"};
-        List<String> two = new ArrayList<>();
-        List<String> three = new ArrayList<>();
-        List<String> four = new ArrayList<>();
         HashMap<Integer, List<String>> map = new HashMap<>();
         for (String s : input) {
-            if (s.length() == 2) {
-                two.add(s);
-                map.put(s.length(), two);
-            }
-            if (s.length() == 3) {
-                three.add(s);
-                map.put(s.length(), three);
-            }
-            if (s.length() == 4) {
-                four.add(s);
-                map.put(s.length(), four);
-            }
+            map.put(s.length(), map.getOrDefault(s.length(), new ArrayList<>()));
+            //can also use map.putIfAbsent(length, new ArrayList<>());
+            map.get(s.length()).add(s);
         }
         System.out.println(map);
     }
